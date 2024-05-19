17c56c622bff31d63f842b07bfdaf3f11d5dac7e
Sakpal Amit <amitsakpal744@gmail.com>
2024-05-19 17:39:35 +0530

    added mapping for users array

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,24 @@ function App() {
   return (
     <>
       <div>
-        <h1>showing users here </h1>
-        <h3>USERS : {users.length}</h3>
+        <h2>showing users here </h2>
+        <p>USERS : {users.length}</p>
+
+        {/*mapping the array of users */ }
+
+        {
+          users.map((users,index)=> {
+
+            <div key={users.id}>
+              <h3>{users.name}</h3>
+              <h3>{users.age}</h3>
+              <h3>{users.role}</h3>
+
+            </div>
+            
+
+          })
+        }
       </div>
     </>
   )
